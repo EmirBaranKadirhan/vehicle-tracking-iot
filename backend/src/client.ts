27d942a005925b61796d4dcbcff150fd96af9b1b
Emir Baran Kadirhan <ebk1013@gmail.com>
2026-03-22 11:21:00 +0300

    feat: add mqtt publish and subscribe example

--- backend/src/client.ts
+++ backend/src/client.ts
@@ -5,3 +5,18 @@ const client = mqtt.connect('ws://127.0.0.1:8888')
 client.on('connect', () => {
     console.log('connected!')
 })
+
+
+client.subscribe('new-user', (err) => {
+
+    if (!err) {
+        client.publish('new-user', 'EBK')
+    }
+
+})
+
+
+client.on('message', (topic, message) => {
+
+    console.log(topic, ':', message.toString())
+})
