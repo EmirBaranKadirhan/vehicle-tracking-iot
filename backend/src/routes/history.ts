7dd10700ca3be0a06d4656190378c5d78de7794f
Emir Baran Kadirhan <ebk1013@gmail.com>
2026-03-25 21:44:42 +0300

    feat: add vehicleId filter to GET /api/history

--- backend/src/routes/history.ts
+++ backend/src/routes/history.ts
@@ -5,12 +5,26 @@ const router = Router()
 
 router.get('/history', async (req, res) => {
     try {
-        const response = await History.find().sort({ createdAt: -1 }).limit(50)
+
+        const vehicleId = req.query.vehicleId
+
+        let response;
+
+        // const query = vehicleId ? { vehicleId } : {}     if blogu yerine bu sekilde de yazabilirdik
+
+        if (vehicleId) {
+            response = await History.find({ vehicleId }).sort({ createdAt: -1 }).limit(50)
+
+        } else {
+            response = await History.find().sort({ createdAt: -1 }).limit(50)
+        }
+
         return res.status(200).json({ message: "Geçmiş Kayıtlar Getirildi!", response })
     } catch (error) {
         return res.status(500).json({ message: "Bir hata oluştu", error })
     }
 })
+
 
 
 export default router
