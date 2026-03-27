24cfe338e14ec6baf70940a1b00ae85a578755ed
Emir Baran Kadirhan <ebk1013@gmail.com>
2026-03-27 22:08:50 +0300

    feat: add axios interceptor for 401 token expiry handling

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -7,5 +7,19 @@ const instance = axios.create({
 });
 
 
+// instance.interceptors.response.use((success),(error)) 
+instance.interceptors.response.use(                         // api'den donen response'u yakalar her sey yolundaysa devam eder degilse (401) donduyse token silip login sayfasina yonlendirir !!
+    (response) => {
+        return response
+    },
+    (error) => {
+        if (error.response?.status === 401) {
+            localStorage.removeItem("token")
+            window.location.href = "/login"
+        }
+        return Promise.reject(error)
+    }
+)
+
 
 export default instance
