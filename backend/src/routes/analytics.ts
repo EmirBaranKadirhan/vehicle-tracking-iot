8722edad07c1ff6e703c15605c736cfbdc349cf8
Emir Baran Kadirhan <ebk1013@gmail.com>
2026-04-02 00:27:37 +0300

    feat: AI summary endpoint added with Groq integration

--- backend/src/routes/analytics.ts
+++ backend/src/routes/analytics.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import Alert from '../models/Alerts'
+import { analyzeAlerts } from '../services/groqService'
 
 const router = Router()
 
@@ -58,6 +59,79 @@ router.get("/analytics", async (req, res) => {
     } catch (error) {
 
         return res.status(500).json({ message: "Bir hata oluştu", error })
+    }
+
+})
+
+
+router.post("/analytics/ai-summary", async (req, res) => {
+
+    try {
+
+        const sevenDaysAgo = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)
+
+        const speedViolationCount = await Alert.countDocuments({
+            type: 'speed_violation',
+            createdAt: { $gte: sevenDaysAgo }
+        })
+
+        const offlineCount = await Alert.countDocuments({
+            type: 'offline',
+            createdAt: { $gte: sevenDaysAgo }
+        })
+
+        const idleCount = await Alert.countDocuments({
+            type: 'idle',
+            createdAt: { $gte: sevenDaysAgo }
+        })
+
+        const result = await Alert.aggregate([
+            {
+                $match: {
+                    createdAt: { $gte: sevenDaysAgo }
+                }
+            },
+            {
+                $group: {
+                    _id: "$vehicleId",
+                    speedViolations: {
+                        $sum: { $cond: [{ $eq: ["$type", "speed_violation"] }, 1, 0] }
+                    },
+                    offlineCount: {
+                        $sum: { $cond: [{ $eq: ["$type", "offline"] }, 1, 0] }
+                    },
+                    idleCount: {
+                        $sum: { $cond: [{ $eq: ["$type", "idle"] }, 1, 0] }
+                    }
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    vehicleId: "$_id",
+                    speedViolations: 1,
+                    offlineCount: 1,
+                    idleCount: 1
+
+                }
+            }
+        ])
+
+        const aiResponse = await analyzeAlerts({
+            speed_violation: speedViolationCount,
+            offline: offlineCount,
+            idle: idleCount,
+        },
+            result)    // araç bazlı aggregate sonucu 
+
+        if (!aiResponse) {
+            return res.status(500).json({ message: "AI yanıt vermedi" })
+        }
+        const parsed = JSON.parse(aiResponse)
+        return res.status(200).json({ cards: parsed })
+
+    } catch (error) {
+        return res.status(500).json({ message: "AI analiz hatası", error })
     }
 
 })
