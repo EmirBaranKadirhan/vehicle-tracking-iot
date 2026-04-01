d150512cb564f63d4e2c96a383c571fadddcb716
Emir Baran Kadirhan <ebk1013@gmail.com>
2026-04-01 23:01:02 +0300

    feat: added analyticsPage router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
 import { Route, Routes } from "react-router"
 import Dashboard from "./pages/Dashboard"
 import HistoryPage from "./pages/HistoryPage"
+import RegisterPage from "./pages/RegisterPage"
+import LoginPage from "./pages/LoginPage"
+import ProtectedRoute from "./components/ProtectedRoute"
+import AlertsPage from "./pages/AlertsPage"
+import AnalyticsPage from "./pages/AnalyticsPage"
+
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/historical" element={<HistoryPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/historical" element={<HistoryPage />} />
+        <Route path="/alerts" element={<AlertsPage />} />
+        <Route path="/analytics" element={<AnalyticsPage />} />
+      </Route>
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
     </Routes>
   )
 }
